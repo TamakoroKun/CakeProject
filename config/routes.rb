6da1f39efddb60342943c3752f32cad075942d35
i29ri <g1g1yuto@gmail.com>
2020-12-16 03:35:55 +0000

    brachtest

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :customers
+
+    scope module: :public do
+      get "customers/my_page" => "customers#show"
+      get "customers/edit" => "customers#edit"
+      patch "customers" => "customers#update"
+      get "customers/unsubscribe" => "customers#unsubscribe"
+      patch "customers/withdraw" => "customers#withdraw"
+    end
+
+    namespace :admin do
+      resources :customer, :except => [:destroy, :new, :create]
+    end
+
+    scope module: :public do
+      resources :addresses,  :except => :new
+    end
+  
 end
