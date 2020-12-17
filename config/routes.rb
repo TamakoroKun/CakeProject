1995621f2729b0ab605b51f926ef9c0d9a21d293
i29ri <g1g1yuto@gmail.com>
2020-12-17 02:14:04 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,40 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+  end
+
+  namespace :public do
+    get 'addresses/edit'
+  end
+
+  namespace :public do
+    get 'customers/show'
+  end
+
+  namespace :public do
+    get 'customers/edit'
+  end
+
+  namespace :public do
+    get 'customers/unsubscribe'
+  end
+
   devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  scope module: :public do
+    get "customers/my_page" => "customers#show"
+    get "customers/edit" => "customers#edit"
+    patch "customers" => "customers#update"
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"
+  end
+  
+  namespace :admin do
+    resources :customer, :except => [:destroy, :new, :create]
+  end
+  
+  scope module: :public do
+    resources :addresses,  :except => :new
+  end
+  
 end
